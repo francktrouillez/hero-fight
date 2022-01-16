1a456aeedbf46acea6456e52969daac09e9592d3
Kcnarf-exe <francktrouillez99@gmail.com>
2022-01-16 15:31:18 +0100

    bumpmap shadows remove irrelevant comments

--- src/view/webgl/Objects/ComplexObjects/Bumpmap/BumpmappingShadowObject.js
+++ src/view/webgl/Objects/ComplexObjects/Bumpmap/BumpmappingShadowObject.js
@@ -31,20 +31,6 @@ class BumpmappingShadowObject extends BumpmappingObject{
     
     // attach it to the framebuffer
     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, unused_texture, 0);
-    /*this.gl.bindTexture(this.gl.TEXTURE_2D, this.depth_texture);
-    this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.depth_texture_size, this.depth_texture_size, 0, this.gl.RGBA, this.gl.UNSIGNED_BYTE, null);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
-
-    this.frame_buffer = this.gl.createFramebuffer();
-    this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.frame_buffer);
-    this.gl.framebufferTexture2D(this.gl.FRAMEBUFFER, this.gl.COLOR_ATTACHMENT0, this.gl.TEXTURE_2D, this.depth_texture, 0);
-
-    this.depth_buffer = this.gl.createRenderbuffer();
-    this.gl.bindRenderbuffer(this.gl.RENDERBUFFER, this.depth_buffer);
-    this.gl.renderbufferStorage(this.gl.RENDERBUFFER, this.gl.DEPTH_COMPONENT16, this.depth_texture_size, this.depth_texture_size);
-    this.gl.framebufferRenderbuffer(this.gl.FRAMEBUFFER, this.gl.DEPTH_ATTACHMENT, this.gl.RENDERBUFFER, this.depth_buffer);*/
   }
 
   activate_frame_buffer() {
@@ -54,8 +40,6 @@ class BumpmappingShadowObject extends BumpmappingObject{
     this.gl.clearColor(0.2, 0.2, 0.2, 1);
     this.gl.clearDepth(1.0); 
     this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
-    /*this.gl.enable(this.gl.CULL_FACE);
-    this.gl.enable(this.gl.DEPTH_TEST);   */
 
 
   }
